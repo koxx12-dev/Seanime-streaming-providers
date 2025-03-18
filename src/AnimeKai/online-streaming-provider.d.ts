fb4b8d4c23cc7620a511a63d3e9f232d33b7294a
kRYstall9 <antonio99044@hotmail.it>
2025-03-18 11:51:50 +0100

    Added AnimeKai as streaming source

---
+++ src/AnimeKai/online-streaming-provider.d.ts
@@ -0,0 +1,47 @@
+// online-streaming-provider.d.ts
+
+declare type SearchResult = {
+    id: string // Passed to findEpisode
+    title: string
+    url: string
+    subOrDub: SubOrDub
+}
+ 
+declare type SubOrDub = "sub" | "dub" | "both"
+ 
+// Passed to findEpisodeServer
+declare type EpisodeDetails = {
+    id: string
+    // 1, 2, 3, etc.
+    number: number
+    url: string
+    title?: string
+}
+ 
+// Server that hosts the video.
+declare type EpisodeServer = {
+    server: string
+    headers: { [key: string]: string }
+    videoSources: VideoSource[]
+}
+ 
+declare type VideoSourceType = "mp4" | "m3u8"
+ 
+declare type VideoSource = {
+    url: string
+    type: VideoSourceType
+    quality: string
+    subtitles: VideoSubtitle[]
+}
+ 
+declare type VideoSubtitle = {
+    id: string
+    url: string
+    language: string
+    isDefault: boolean
+}
+ 
+declare type Settings = {
+    episodeServers: string[]
+    supportsDub: boolean
+}
