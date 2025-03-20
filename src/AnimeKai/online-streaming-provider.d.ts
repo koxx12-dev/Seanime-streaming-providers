da67fc9fc13808a2d52efcdcdb210f3307722700
kRYstall9 <antonio99044@hotmail.it>
2025-03-20 23:10:31 +0100

    updated online-streaming-provider.d.ts

--- src/AnimeKai/online-streaming-provider.d.ts
+++ src/AnimeKai/online-streaming-provider.d.ts
@@ -1,53 +1,60 @@
 // online-streaming-provider.d.ts
 
 declare type SearchResult = {
-    id: string // Passed to findEpisode
+    id: string
     title: string
     url: string
     subOrDub: SubOrDub
 }
- 
+
 declare type SubOrDub = "sub" | "dub" | "both"
- 
-// Passed to findEpisodeServer
+
 declare type EpisodeDetails = {
     id: string
-    // 1, 2, 3, etc.
     number: number
     url: string
     title?: string
 }
- 
-// Server that hosts the video.
+
 declare type EpisodeServer = {
     server: string
     headers: { [key: string]: string }
     videoSources: VideoSource[]
 }
- 
+
 declare type VideoSourceType = "mp4" | "m3u8"
- 
+
 declare type VideoSource = {
     url: string
     type: VideoSourceType
     quality: string
     subtitles: VideoSubtitle[]
 }
- 
+
 declare type VideoSubtitle = {
     id: string
     url: string
     language: string
     isDefault: boolean
 }
- 
-declare type Settings = {
-    episodeServers: string[]
-    supportsDub: boolean
-}
 
 declare type SearchOptions = {
     query: string
     dub: boolean
     year?: number
+}
+
+declare type Settings = {
+    episodeServers: string[]
+    supportsDub: boolean
+}
+
+declare abstract class AnimeProvider {
+    search(opts: SearchOptions): Promise<SearchResult[]>
+
+    findEpisodes(id: string): Promise<EpisodeDetails[]>
+
+    findEpisodeServer(episode: EpisodeDetails, server: string): Promise<EpisodeServer>
+
+    getSettings(): Settings
 }
